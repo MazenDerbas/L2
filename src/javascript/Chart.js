00323ecfabe638181ef039410b7b41d31c6aa6e1
md222zv <89812908+Mazender@users.noreply.github.com>
2023-09-24 22:30:45 +0200

    Add test rapport

--- src/javascript/Chart.js
+++ src/javascript/Chart.js
@@ -18,36 +18,43 @@ export class Chart {
    * Draws a bar chart on the canvas.
    */
   drawBarChart() {
-      const PADDING_BOTTOM = 20;
-      const PADDING_TOP = 40;
-      const BAR_WIDTH = 60;
-      const SPACE_BETWEEN_BARS = 80;
-      const LABEL_OFFSET_X = 20 / 2; // Half of 20 to center the label
-      const LABEL_OFFSET_Y = 5;
-
-      let maxValue = 0;
-
-      // Find the maximum value in the dataset
-      for (let i = 0; i < this.data.length; i++) {
-          const item = this.data[i];
-          if (item.value > maxValue) {
-              maxValue = item.value;
-          }
-      }
-
-      const chartHeight = this.canvas.height - PADDING_TOP;
-
-      // Draw each bar based on its value
-      for (let i = 0; i < this.data.length; i++) {
-          const item = this.data[i];
-          const x = PADDING_TOP + i * SPACE_BETWEEN_BARS;
-
-          const barHeight = (item.value / maxValue) * chartHeight;
-          const y = this.canvas.height - PADDING_BOTTOM - barHeight;
-
-          this.ctx.fillStyle = 'black';
-          this.ctx.fillRect(x, y, BAR_WIDTH, barHeight);
-          this.ctx.fillText(item.label, x + LABEL_OFFSET_X, this.canvas.height - LABEL_OFFSET_Y);
-      }
+    const PADDING_BOTTOM = 20;
+    const PADDING_TOP = 40;
+    const BAR_WIDTH = 60;
+    const SPACE_BETWEEN_BARS = 80;
+    const LABEL_OFFSET_X = 20 / 2; // Half of 20 to center the label
+    const LABEL_OFFSET_Y = 5;
+    let maxValue = 0;
+    // Find the maximum value in the dataset
+    for (let i = 0; i < this.data.length; i++) {
+        const item = this.data[i];
+        if (item.value > maxValue) {
+            maxValue = item.value;
+        }
+    }
+    const chartHeight = this.canvas.height - PADDING_TOP;
+    // Draw each bar based on its value
+    for (let i = 0; i < this.data.length; i++) {
+        const item = this.data[i];
+        const x = PADDING_TOP + i * SPACE_BETWEEN_BARS;
+        const barHeight = (item.value / maxValue) * chartHeight;
+        const y = this.canvas.height - PADDING_BOTTOM - barHeight;
+        this.ctx.fillStyle = this.generateRandomColor();;
+        this.ctx.fillRect(x, y, BAR_WIDTH, barHeight);
+        this.ctx.fillText(item.label, x + LABEL_OFFSET_X, this.canvas.height - LABEL_OFFSET_Y);
+    }
+  }
+  
+  
+  /**
+  * Generate a random RGB colors.
+  * 
+  * @returns {string} A string representing the random RGB color in the format "rgb(r,g,b)".
+  */
+  generateRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
   }
 }
---
+++ src/testrapport.md
@@ -0,0 +1,19 @@
+# Test Report
+
+| Test Name                                                      | Status  | Type     |
+|---------------------------------------------------------------|:-------:|:--------:|
+| addExpense should add an expense to the list                  |    ✓    | Auto     |
+| addExpense should add a new category if it does not exist     |    ✓    | Auto     |
+| getExpensesByCategory should return expenses for a specific category | ✓ | Auto |
+| getTotalExpenses should return the total expenses of all categories | ✓ | Auto |
+| getCategoryExpenses should return the total expenses of a category | ✓ | Auto |
+| getExpenseReport should return summary of all expenses for all the categories | ✓ | Auto |
+| addBudget should add a budget to the list                     |    ✓    | Auto     |
+| Calculate the remaining budget for a category                 |    ✓    | Auto     |
+| getExpenseReport should return summary of all expenses for all the categories | ✓ | Auto |
+| getBudgetReport should return budget report with remaining amounts | ✓ | Auto |
+| getExpensesByDate should return expenses for a specific date  |    ✓    | Auto     |
+| getExpensesByDateInterval should return expenses within a date interval | ✓ | Auto |
+| removeExpense method should remove an existing expense from the list | ✓ | Auto |
+| removeBudget method should remove an existing expense from the list | ✓ | Auto |
+
