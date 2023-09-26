c46da71e2974d71c937289cb3c933f4a4f074858
md222zv <89812908+Mazender@users.noreply.github.com>
2023-09-26 18:09:17 +0200

    Fix lint

--- src/javascript/Budget.js
+++ src/javascript/Budget.js
@@ -1,49 +1,54 @@
 /**
  * Represents a budget for a specific category.
  */
-export class Budget{
-    #category
-    #amount
+export class Budget {
+  #category
+  #amount
 
-    /**
-     * Creates a new Budget instance.
-     * @param {string} category - The category for which the budget is defined.
-     * @param {number} amount - The budgeted amount for the specified category.
-     */
-    constructor(category, amount) {
-        this.#setCategory(category);
-        this.#setAmount(amount);
-    }
+  /**
+   * Creates a new Budget instance.
+   *
+   * @param {string} category - The category for which the budget is defined.
+   * @param {number} amount - The budgeted amount for the specified category.
+   */
+  constructor (category, amount) {
+    this.#setCategory(category)
+    this.#setAmount(amount)
+  }
 
-    /**
-     * Sets the category for which the budget is defined.
-     * @param {string} category - The category name.
-     */
-    #setCategory(category) {
-        this.#category = category;
-    }
+  /**
+   * Sets the category for which the budget is defined.
+   *
+   * @param {string} category - The category name.
+   */
+  #setCategory (category) {
+    this.#category = category
+  }
 
-    /**
-     * Sets the budgeted amount for the category.
-     * @param {number} amount - The budgeted amount.
-     */
-    #setAmount(amount) {
-        this.#amount = amount;
-    }
-    
-    /**
-     * Gets the category for which the budget is defined.
-     * @returns {string} - The category name.
-     */
-    getCategory() {
-        return this.#category;
-    }
+  /**
+   * Sets the budgeted amount for the category.
+   *
+   * @param {number} amount - The budgeted amount.
+   */
+  #setAmount (amount) {
+    this.#amount = amount
+  }
 
-    /**
-     * Gets the budgeted amount for the category.
-     * @returns {number} - The budgeted amount.
-     */
-    getAmount() {
-        return this.#amount;
-    }
+  /**
+   * Gets the category for which the budget is defined.
+   *
+   * @returns {string} - The category name.
+   */
+  getCategory () {
+    return this.#category
+  }
+
+  /**
+   * Gets the budgeted amount for the category.
+   *
+   * @returns {number} - The budgeted amount.
+   */
+  getAmount () {
+    return this.#amount
+  }
 }
--- test-app/index.test.js
+++ test-app/index.test.js
@@ -1,176 +1,179 @@
+/* eslint-disable no-undef */
 // ExpenseTracker.test.js
 
-import { ExpenseTracker } from "../src/javascript/ExpenseTracker.js";
-
+import { ExpenseTracker } from '../src/javascript/ExpenseTracker.js'
+import { Chart } from '../src/javascript/chart.js'
 
 describe('ExpenseTracker', () => {
-  let tracker;
+  let tracker
 
   beforeEach(() => {
-    tracker = new ExpenseTracker();
-  });
+    tracker = new ExpenseTracker()
+  })
 
   test('addExpense should add an expense to the list', () => {
-    const initialExpensesCount = tracker.getExpensList().length;
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A');
-    const updatedExpensesCount = tracker.getExpensList().length;
+    const initialExpensesCount = tracker.getExpensList().length
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A')
+    const updatedExpensesCount = tracker.getExpensList().length
 
-    expect(updatedExpensesCount).toBe(initialExpensesCount + 1);
-  });
-
+    expect(updatedExpensesCount).toBe(initialExpensesCount + 1)
+  })
 
   test('addExpense should add a new category if it does not exist', () => {
-    const initialCategoryCount = tracker.getCategoryList().length;
+    const initialCategoryCount = tracker.getCategoryList().length
 
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'New Category');
-    const updatedCategoryCount = tracker.getCategoryList().length;
-    expect(updatedCategoryCount).toBe(initialCategoryCount + 1);
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'New Category')
+    const updatedCategoryCount = tracker.getCategoryList().length
+    expect(updatedCategoryCount).toBe(initialCategoryCount + 1)
 
-    const newCategoryExists = tracker.getCategoryList().some(category => category.getName() === 'New Category');
-    expect(newCategoryExists).toBe(true);
-  });
-
+    const newCategoryExists = tracker.getCategoryList().some(category => category.getName() === 'New Category')
+    expect(newCategoryExists).toBe(true)
+  })
 
   test('getExpensesByCategory should return expenses for a specific category', () => {
     // Add expenses with different categories
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B');
-    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A');
-    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C');
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B')
+    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A')
+    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C')
 
-    const categoryAExpenses = tracker.getExpensesByCategory('Category A');
-    expect(categoryAExpenses[0].getName()).toBe('Expense 1');
-    expect(categoryAExpenses[1].getName()).toBe('Expense 3');
-  
-  });
+    const categoryAExpenses = tracker.getExpensesByCategory('Category A')
+    expect(categoryAExpenses[0].getName()).toBe('Expense 1')
+    expect(categoryAExpenses[1].getName()).toBe('Expense 3')
+  })
 
   test('getTotalExpenses should return the total expenses of all categories', () => {
     // Add expenses with different categories
 
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B');
-    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A');
-    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C');
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B')
+    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A')
+    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C')
 
     const categoryAExpenses = tracker.getTotalExpenses()
 
-    expect(categoryAExpenses).toBe(265);
- 
-    
-  });
+    expect(categoryAExpenses).toBe(265)
+  })
 
   test('getCategoryExpenses should return the total expenses of a category', () => {
     // Add expenses with different categories
 
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B');
-    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A');
-    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C');
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B')
+    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A')
+    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C')
 
     const categoryAExpenses = tracker.getExpenesAmountByCategory('Category A')
-    expect(categoryAExpenses).toBe(110);
-    
-  });
+    expect(categoryAExpenses).toBe(110)
+  })
 
   test('getExpenseReport should return summary of all expenses for all the categories', () => {
     // Add expenses with different categories
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B');
-    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A');
-    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C');
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B')
+    tracker.addExpense('Expense 3', 60, '2023-09-17', 'Category A')
+    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C')
 
     const categoryAExpenses = tracker.getExpenseReport()
     expect(categoryAExpenses).toEqual({
-      "Category A" : 110,
-      "Category B" : 75 ,
-      "Category C" : 80
+      'Category A': 110,
+      'Category B': 75,
+      'Category C': 80
     })
-  });
+  })
 
   test('addBudget should add a budget to the list', () => {
-    const initialBudgetsCount = tracker.getBudgetList().length;
-    tracker.addBudget('Category A', 1000); // Add a budget for 'Category A'
-    const updatedBudgetsCount = tracker.getBudgetList().length;
+    const initialBudgetsCount = tracker.getBudgetList().length
+    tracker.addBudget('Category A', 1000) // Add a budget for 'Category A'
+    const updatedBudgetsCount = tracker.getBudgetList().length
 
-    expect(updatedBudgetsCount).toBe(initialBudgetsCount + 1);
-  }); 
+    expect(updatedBudgetsCount).toBe(initialBudgetsCount + 1)
+  })
 
-  test('Calculate the remaning budget for a category' , () => {
+  test('Calculate the remaning budget for a category', () => {
     // Add expenses with different categories
 
     tracker.addBudget('Category A', 500)
-    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B');
-    tracker.addExpense('Expense 3', 100, '2023-09-17', 'Category A');
-    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C');
+    tracker.addExpense('Expense 1', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense 2', 75, '2023-09-16', 'Category B')
+    tracker.addExpense('Expense 3', 100, '2023-09-17', 'Category A')
+    tracker.addExpense('Expense 4', 80, '2023-09-18', 'Category C')
 
     const remainBudget = tracker.getRemainingBudget('Category A')
     expect(remainBudget).toBe(350)
 
     expect(() => {
-      tracker.getRemainingBudget('Category D');
-
+      tracker.getRemainingBudget('Category D')
     }).toThrowError('Category D does not exist in the category list')
-
-  });
+  })
 
   test('getExpenseReport should return summary of all expenses for all the categories', () => {
     // Add expenses
-    tracker.addExpense('Expense A', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense B', 75, '2023-09-16', 'Category B');
-    
-    const report = tracker.getExpenseReport();
-    expect(report['Category A']).toBe(50);
-    expect(report['Category B']).toBe(75);
-  });
-  
+    tracker.addExpense('Expense A', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense B', 75, '2023-09-16', 'Category B')
+    const report = tracker.getExpenseReport()
+    expect(report['Category A']).toBe(50)
+    expect(report['Category B']).toBe(75)
+  })
 
   test('getBudgetReport should return budget report with remaining amounts', () => {
-    tracker.addBudget('Category A', 500);
-    tracker.addExpense('Expense A', 50, '2023-09-15', 'Category A');
-    
-    const report = tracker.getBudgetReport();
-    expect(report['Category A'].totalBudget).toBe(500);
-    expect(report['Category A'].remainingBudget).toBe(450);
-  });
-  
+    tracker.addBudget('Category A', 500)
+    tracker.addExpense('Expense A', 50, '2023-09-15', 'Category A')
+    const report = tracker.getBudgetReport()
+    expect(report['Category A'].totalBudget).toBe(500)
+    expect(report['Category A'].remainingBudget).toBe(450)
+  })
 
   test('getExpensesByDate should return expenses for a specific date', () => {
-    tracker.addExpense('Expense A', 50, '2023-09-15', 'Category A');
-    tracker.addExpense('Expense B', 75, '2023-09-16', 'Category B');
-    
-    const expenses = tracker.getExpensesByDate('2023-09-15');
-    expect(expenses.length).toBe(1);
-    expect(expenses[0].getName()).toBe('Expense A');
-  });
+    tracker.addExpense('Expense A', 50, '2023-09-15', 'Category A')
+    tracker.addExpense('Expense B', 75, '2023-09-16', 'Category B')
+
+    const expenses = tracker.getExpensesByDate('2023-09-15')
+    expect(expenses.length).toBe(1)
+    expect(expenses[0].getName()).toBe('Expense A')
+  })
 
   test('getExpensesByDateInterval should return expenses within a date interval', () => {
-    tracker.addExpense('Expense A', 50, '2023-09-14', 'Category A');
-    tracker.addExpense('Expense B', 75, '2023-09-15', 'Category B');
-    tracker.addExpense('Expense C', 100, '2023-09-16', 'Category C');
-    
-    const expenses = tracker.getExpensesByDateInterval('2023-09-14', '2023-09-15');
-    expect(expenses.length).toBe(2);
-    expect(expenses[0].getName()).toBe('Expense A');
-    expect(expenses[1].getName()).toBe('Expense B');
-  });
+    tracker.addExpense('Expense A', 50, '2023-09-14', 'Category A')
+    tracker.addExpense('Expense B', 75, '2023-09-15', 'Category B')
+    tracker.addExpense('Expense C', 100, '2023-09-16', 'Category C')
+
+    const expenses = tracker.getExpensesByDateInterval('2023-09-14', '2023-09-15')
+    expect(expenses.length).toBe(2)
+    expect(expenses[0].getName()).toBe('Expense A')
+    expect(expenses[1].getName()).toBe('Expense B')
+  })
 
   test('removeExpense method should remove an existing expense from the list', () => {
-
-    tracker.addExpense('Expense A', 50, '2023-09-14', 'Category A');
-    tracker.addExpense('Expense B', 75, '2023-09-15', 'Category B');
-    tracker.addExpense('Expense C', 100, '2023-09-16', 'Category C');
+    tracker.addExpense('Expense A', 50, '2023-09-14', 'Category A')
+    tracker.addExpense('Expense B', 75, '2023-09-15', 'Category B')
+    tracker.addExpense('Expense C', 100, '2023-09-16', 'Category C')
 
     tracker.removeExpense('Expense A')
-    expect(tracker.getExpensList().length).toBe(2);
-  });
-  
-  test('removeBudget method should remove an existing expense from the list', () => {
+    expect(tracker.getExpensList().length).toBe(2)
+  })
 
-    tracker.addBudget('Category A', 500);
-    tracker.addBudget('Category B', 500);
-  
+  test('removeBudget method should remove an existing expense from the list', () => {
+    tracker.addBudget('Category A', 500)
+    tracker.addBudget('Category B', 500)
+
     tracker.removeBudget('Category A')
-    expect(tracker.getBudgetList().length).toBe(1);
-  });
-});
+    expect(tracker.getBudgetList().length).toBe(1)
+  })
+})
+
+tracker.addExpense('Expense A', 50, '2023-09-14', 'Category A')
+tracker.addExpense('Expense B', 75, '2023-09-15', 'Category B')
+tracker.addExpense('Expense C', 100, '2023-09-16', 'Category C')
+
+const containerId = 'barChart'
+const expensesInAugust = tracker.getExpensesByDateInterval('2023-09-10', '2023-09-12')
+
+const dateChartData = expensesInAugust.map(expense => ({
+  label: expense.getName(),
+  value: expense.getAmount()
+}))
+
+// Draw the chart
+const dateChart = new Chart(containerId, dateChartData)
+dateChart.drawBarChart()
